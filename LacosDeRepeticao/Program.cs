9d2091bca22d3fcfad44b880480516d3c64e9681
MarceloZim <marcelod232015@gmail.com>
2022-01-15 17:42:01 -0300

    Aula2 Att2

--- LacosDeRepeticao/Program.cs
+++ LacosDeRepeticao/Program.cs
@@ -5,8 +5,25 @@ namespace LacosDeRepeticao
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {/*
+          ################# DO WHILE ####################
+            int n = 1;
+            while (n <= 10)
+            {
+                Console.WriteLine(String.Format("Valor corrente de n é: {0}", n));
+                n++;
+            }
+            Console.Read();*/
+
+
+
+            int[] arrayDeInteiros = new int[] { 0, 1, 1, 2, 3, 5, 8, 13 };
+            foreach (int inteiro in arrayDeInteiros)
+            {
+                Console.WriteLine(inteiro);
+            }
+            Console.Read();
+
         }
     }
 }
